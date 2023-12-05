9d9288d7b3294b3558e25ed600180026c7b3b2bc
lksmaxx <lucasmaximinotorres@gmail.com>
2023-12-04 22:06:54 -0300

    basic buffer methods

--- include/renderer/renderer.hpp
+++ include/renderer/renderer.hpp
@@ -8,7 +8,19 @@ public:
   Renderer(const Window &window);
   ~Renderer();
 
+  // screen
   void clearScreen();
+
+  // buffers
+
+  // VAO
+  int generateVertexArray();
+  void bindVertexArray(const int vao) const;
+
+  // VBO
+  int generateVertexBuffer();
+  void bindVertexBuffer(const int vbo) const;
+  void bufferArrayStaticData(size_t data_size, const void *data) const;
 
 private:
   const Window &m_window;
--- src/renderer/renderer.cpp
+++ src/renderer/renderer.cpp
@@ -10,4 +10,23 @@ void Renderer::clearScreen() {
   glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
   glClear(GL_COLOR_BUFFER_BIT);
 }
+
+int Renderer::generateVertexArray() {
+  unsigned int vao;
+  glGenVertexArrays(1, &vao);
+  return vao;
+}
+
+void Renderer::bindVertexArray(const int vao) const { glBindVertexArray(vao); }
+
+int Renderer::generateVertexBuffer() {
+  unsigned int vbo;
+  glGenBuffers(1, &vbo);
+  return vbo;
+}
+
+void Renderer::bufferArrayStaticData(size_t data_size, const void *data) const {
+  glBufferData(GL_ARRAY_BUFFER, data_size, data, GL_STATIC_DRAW);
+}
+
 } // namespace Engine
